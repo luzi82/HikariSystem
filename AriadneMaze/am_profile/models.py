d6e59457e77e26667cfd8ff0b3d49ebbc70ba67a
Luzi82 <luzi82@gmail.com>
2014-01-25 11:12:44 +0800

    simple profile

--- AriadneMaze/am_profile/models.py
+++ AriadneMaze/am_profile/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import models as auth_models
 
-# Create your models here.
+PROFILE_NAME_LENGTH = 64
+
+class AmProfile(models.Model):
+
+    user = models.ForeignKey(auth_models.User,db_index=True)
+    name = models.CharField(max_length=PROFILE_NAME_LENGTH,db_index=True)
