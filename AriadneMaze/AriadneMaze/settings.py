f769573217c7a012f1d7a61f75d4d0fd0e25de39
Luzi82 <luzi82@gmail.com>
2014-01-25 10:31:44 +0800

    south

--- AriadneMaze/AriadneMaze/settings.py
+++ AriadneMaze/AriadneMaze/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'am_profile',
+    
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
