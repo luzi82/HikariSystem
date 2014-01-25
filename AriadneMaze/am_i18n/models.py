2cdd84b81d721bb07e1f268ac3114f53f159ac38
Luzi82 <luzi82@gmail.com>
2014-01-25 11:34:37 +0800

    i18n, hell yeah ppl love i18n

--- AriadneMaze/am_i18n/models.py
+++ AriadneMaze/am_i18n/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+LANG_NAME_LENGTH = 16
+
 # Create your models here.
+
+class AmLang(models.Model):
+    
+    name = models.CharField(max_length=LANG_NAME_LENGTH,db_index=True)
+
+class AmTextKey(models.Model):
+    
+    info = models.TextField()
+
+class AmTextI18n(models.Model):
+    
+    lang = models.ForeignKey(AmLang,db_index=True)
+    text_key = models.ForeignKey(AmTextKey,db_index=True)
+    text = models.TextField()
