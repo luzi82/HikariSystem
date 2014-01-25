a3a886dd245bd67791bc56dba0781ec902397b64
Luzi82 <luzi82@gmail.com>
2014-01-25 12:00:18 +0800

    level models

---
+++ AriadneMaze/am_level/migrations/0002_auto__add_amuserlevel__add_amleveltype__add_amlevelexp.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'AmUserLevel'
+        db.create_table(u'am_level_amuserlevel', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['am_level.AmLevelType'])),
+            ('exp', self.gf('django.db.models.fields.IntegerField')(db_index=True)),
+        ))
+        db.send_create_signal(u'am_level', ['AmUserLevel'])
+
+        # Adding model 'AmLevelType'
+        db.create_table(u'am_level_amleveltype', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['am_i18n.AmTextKey'])),
+        ))
+        db.send_create_signal(u'am_level', ['AmLevelType'])
+
+        # Adding model 'AmLevelExp'
+        db.create_table(u'am_level_amlevelexp', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['am_level.AmLevelType'])),
+            ('level', self.gf('django.db.models.fields.IntegerField')(db_index=True)),
+            ('exp_min', self.gf('django.db.models.fields.IntegerField')(db_index=True)),
+        ))
+        db.send_create_signal(u'am_level', ['AmLevelExp'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'AmUserLevel'
+        db.delete_table(u'am_level_amuserlevel')
+
+        # Deleting model 'AmLevelType'
+        db.delete_table(u'am_level_amleveltype')
+
+        # Deleting model 'AmLevelExp'
+        db.delete_table(u'am_level_amlevelexp')
+
+
+    models = {
+        u'am_i18n.amtextkey': {
+            'Meta': {'object_name': 'AmTextKey'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'info': ('django.db.models.fields.TextField', [], {}),
+            'key': ('django.db.models.fields.CharField', [], {'max_length': '64', 'db_index': 'True'})
+        },
+        u'am_level.amlevelexp': {
+            'Meta': {'object_name': 'AmLevelExp'},
+            'exp_min': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'level': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
+            'type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['am_level.AmLevelType']"})
+        },
+        u'am_level.amleveltype': {
+            'Meta': {'object_name': 'AmLevelType'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['am_i18n.AmTextKey']"})
+        },
+        u'am_level.amuserlevel': {
+            'Meta': {'object_name': 'AmUserLevel'},
+            'exp': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['am_level.AmLevelType']"}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
+        },
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['am_level']
--- AriadneMaze/am_level/models.py
+++ AriadneMaze/am_level/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import am_i18n.models as i18n_models
+from django.contrib.auth import models as auth_models
 
-# Create your models here.
+class AmLevelType(models.Model):
+    
+    name = models.ForeignKey(i18n_models.AmTextKey)
+
+class AmLevelExp(models.Model):
+
+    type = models.ForeignKey(AmLevelType,db_index=True)
+    level = models.IntegerField(db_index=True)
+    exp_min = models.IntegerField(db_index=True)
+
+class AmUserLevel(models.Model):
+    
+    user = models.ForeignKey(auth_models.User,db_index=True)
+    type = models.ForeignKey(AmLevelType,db_index=True)
+    exp = models.IntegerField(db_index=True)
