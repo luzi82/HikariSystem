178b1400a8c812a2c908c0e53ad110fbe9696e0b
Luzi82 <luzi82@gmail.com>
2014-02-23 14:21:34 +0800

    change lib path

--- gen_protocol.sh
+++ gen_protocol.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd HikariClientProtocolGen
-java -cp "bin:lib/commons-collections-3.2.1.jar:lib/commons-io-2.4.jar:lib/commons-lang-2.4.jar:lib/commons-lang3-3.2.1.jar:lib/velocity-1.7.jar:../HikariProtocolDef/bin" com.luzi82.hikari.client.protocol.gen.Main
+java -cp "bin:../import/lib/commons-collections-3.2.1.jar:../import/lib/commons-io-2.4.jar:../import/lib/commons-lang-2.4.jar:../import/lib/commons-lang3-3.2.1.jar:lib/velocity-1.7.jar:../HikariProtocolDef/bin" com.luzi82.hikari.client.protocol.gen.Main
 
