aed5fabdbf4cc2caaecfe6693c230ba0c72d55be
Luzi82 <luzi82@gmail.com>
2014-02-23 12:56:33 +0800

    testRetry

--- HikariClientTest/src/com/luzi82/hikari/client/test/CacheTest.java
+++ HikariClientTest/src/com/luzi82/hikari/client/test/CacheTest.java
@@ -1,9 +1,15 @@
 package com.luzi82.hikari.client.test;
 
+import java.util.concurrent.ExecutionException;
+
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.luzi82.concurrent.RecordFutureCallback;
+import com.luzi82.concurrent.RetryableFuture;
 import com.luzi82.hikari.client.HsClient;
+import com.luzi82.hikari.client.protocol.DevelopmentProtocol;
+import com.luzi82.hikari.client.protocol.DevelopmentProtocolDef.PassTimeCmd;
 import com.luzi82.hikari.client.protocol.SystemProtocol;
 
 public class CacheTest extends AbstractTest {
@@ -34,5 +40,48 @@ public class CacheTest extends AbstractTest {
 		Assert.assertEquals(systemTime0, systemTime1);
 
 		Assert.assertEquals(seqId2, client.getCookie("seqid"));
+	}
+
+	@Test
+	public void testRetry() throws Exception {
+		HsClient client = createClient();
+
+		createLogin(client);
+
+		long serverTime = SystemProtocol.getTime(client, null).get().time;
+		long clientTime = System.currentTimeMillis();
+
+		RecordFutureCallback<PassTimeCmd.Result> rfc = new RecordFutureCallback<PassTimeCmd.Result>(
+				null);
+		RetryableFuture<PassTimeCmd.Result> retryableFuture = DevelopmentProtocol
+				.passTime(client, serverTime + 1000, -1, rfc);
+
+		try {
+			retryableFuture.get();
+			Assert.fail();
+		} catch (ExecutionException ee) {
+		}
+
+		Assert.assertFalse(rfc.completed);
+		Assert.assertTrue(rfc.failed);
+		Assert.assertFalse(rfc.cancelled);
+		Assert.assertNull(rfc.result);
+		Assert.assertNotNull(rfc.ex);
+
+		PassTimeCmd.Result result = null;
+		while (System.currentTimeMillis() < clientTime + 2000) {
+			rfc.clear();
+			try {
+				result = retryableFuture.get();
+				break;
+			} catch (ExecutionException ee) {
+			}
+		}
+
+		Assert.assertTrue(rfc.completed);
+		Assert.assertFalse(rfc.failed);
+		Assert.assertFalse(rfc.cancelled);
+		Assert.assertSame(result, rfc.result);
+		Assert.assertNull(rfc.ex);
 	}
 }
---
+++ HikariProtocolDef/src/com/luzi82/hikari/client/protocol/DevelopmentProtocolDef.java
@@ -0,0 +1,15 @@
+package com.luzi82.hikari.client.protocol;
+
+public class DevelopmentProtocolDef {
+
+	public static class PassTimeCmd {
+		public static class Request {
+			public long start;
+			public long end;
+		}
+
+		public static class Result {
+		}
+	}
+
+}
