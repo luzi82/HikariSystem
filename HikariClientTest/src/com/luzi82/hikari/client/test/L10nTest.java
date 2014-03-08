7e9820132f84ace43556ace63822cd4729dee2a5
Luzi82 <luzi82@gmail.com>
2014-03-08 18:34:04 +0800

    L10n test, BAD RED

---
+++ HikariClientTest/src/com/luzi82/hikari/client/test/L10nTest.java
@@ -0,0 +1,25 @@
+package com.luzi82.hikari.client.test;
+
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.luzi82.hikari.client.HsClient;
+
+public class L10nTest extends AbstractTest {
+
+	@Test
+	public void testText() throws Exception {
+		HsClient client = createClient();
+		L10n.setL10n(client, "en");
+		client.syncData(null).get();
+
+		Map<String, String> langTextMap = L10n.getTextMapData(client);
+		Assert.assertEquals("Quest 0", langTextMap.get("quest_entry__quest_0"));
+		Assert.assertEquals("Coin", langTextMap.get("resource__coin"));
+		Assert.assertEquals("Coin to gold",
+				langTextMap.get("resource_convert__coin_to_gold"));
+	}
+
+}
