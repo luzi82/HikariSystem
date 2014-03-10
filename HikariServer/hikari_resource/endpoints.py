a45fe0658e9d115d5232786ac6ebdfa1a3bd445d
Luzi82 <luzi82@gmail.com>
2014-03-10 23:15:42 +0800

    check offset < 0

--- HikariServer/hikari_resource/endpoints.py
+++ HikariServer/hikari_resource/endpoints.py
@@ -70,6 +70,8 @@ def get_convert_history_list(request):
 
     # check
     
+    if offset < 0:
+        raise AJAXError(400, "offset < 0")
     if count <= 0:
         raise AJAXError(400, "count <= 0")
 
